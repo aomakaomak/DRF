249a23473c44051291c3dcbc1f3f8d880277461f
Andrei Makarov <aomak@yandex.ru>
2025-11-20 15:35:11 +0300

    добавили фильтрацию

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
+
     'users',
     'vehicle',
 
--- vehicle/urls.py
+++ vehicle/urls.py
@@ -3,7 +3,7 @@ from vehicle.apps import VehicleConfig
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 
-from vehicle.views import CarViewSet, MotoCreateAPIView, MotoListAPIView, MotoRetrieveAPIView, MotoUpdateAPIView, MotoDestroyAPIView, MotoMilageListAPIView, MilageCreateAPIView
+from vehicle.views import CarViewSet, MotoCreateAPIView, MotoListAPIView, MotoRetrieveAPIView, MotoUpdateAPIView, MotoDestroyAPIView, MotoMilageListAPIView, MilageCreateAPIView, MilageListAPIView
 
 app_name = VehicleConfig.name
 
@@ -17,6 +17,7 @@ urlpatterns = [
     path('moto/update/<int:pk>/', MotoUpdateAPIView.as_view(), name='moto-update'),
     path('moto/delete/<int:pk>/', MotoDestroyAPIView.as_view(), name='moto-delete'),
 
+    path('milage/', MilageListAPIView.as_view(), name='milage-list'),
     path('milage/create/', MilageCreateAPIView.as_view(), name='milage-create'),
     path('moto/milage/', MotoMilageListAPIView.as_view(), name='moto-milage'),
 
